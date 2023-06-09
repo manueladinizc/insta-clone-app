f158a4edea4b1a32261cd5860ee452bf03e729f9
manueladinic <manuela.mdc@gmail.com>
2023-06-09 01:15:13 -0300

    ajust configuration

--- README.md
+++ README.md
@@ -73,4 +73,4 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/t
 
 <!-- https://medium.com/nerd-for-tech/setting-up-a-create-react-app-with-firebase-4fb3601fe2d4 -->
 
-welcome: 
+firebase: 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import { firebase, FieldValue } from './lib/firebase';
+// import FirebaseContext from './context/firebase';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <App />
+//   );
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { firebase, FieldValue } from './lib/firebase';
+import FirebaseContext from './context/firebase';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
+ReactDOM.render(
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+     <App />
+    <h1 className="text-red-500">Hello, World</h1>
+  </FirebaseContext.Provider>,
+  document.getElementById('root')
 );
-
-
-
