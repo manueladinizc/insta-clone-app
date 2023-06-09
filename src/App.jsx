2c9c5f8df3131ac3358858702ac58e8caaf0df47
manueladinic <manuela.mdc@gmail.com>
2023-06-08 23:09:29 -0300

    config tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-red font-bold underline">
+      Hello world!
+    </h1>
+    <h1 className="text-red-500">Hello, World</h1>,
+    </div>
+  );
+}
+
+export default App;
