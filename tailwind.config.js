501e864fd9eafd22e09109b1d4dbd2509e7a166f
manuela.cavalcanti <manuela.cavalcanti@rebase.com.br>
2023-06-17 15:08:51 -0300

    login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,30 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fill: (theme) => ({
+      red: theme('colors.red.primary'),
+    }),
+    colors: {
+      white: '#ffffff',
+      blue: {
+        medium: '#005c98',
+      },
+      black: {
+        light: '#005c98',
+        faded: '#00000059',
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb',
+      },
+      red: {
+        primary: '#ed4956',
+      },
+    },
   },
   plugins: [],
-};
+}
+
+
+
